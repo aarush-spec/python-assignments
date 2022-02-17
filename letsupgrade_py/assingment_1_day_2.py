362bb7bcd24da49bcbd155af41ae1b45ef0ad82d
Aarush Kathuria <80425165+aarush-spec@users.noreply.github.com>
2022-02-18 02:08:53 +0530

    Add files via upload

---
+++ letsupgrade_py/assingment_1_day_2.py
@@ -0,0 +1,73 @@
+# QUESTION 1
+# Answer :- IF ELIF ELSE
+
+
+
+# QUESTION 2
+# Answer :-
+# if(condition):
+#     (statement)
+
+
+
+
+# QUESTION 3
+# Answer:-
+# print("HELLO HOW ARE U");
+# age = int(input("WHAT IS YOUR AGE ? "))
+# if (age >= 18):
+#     print("You are eligible for voting. ")
+# else:
+#     print("Sorry You are not eligible for voting. ")
+
+
+
+# QUESTION 4
+# Answer:-
+# Know Program
+
+
+
+
+# QUESTION 5
+# Answer:-
+# number = int(input("Please enter a number. "))
+# if(number % 2 ==0):
+#     print("It is an even number. ")
+# else:
+#     print("It is an odd number. ")
+
+
+
+
+# QUESTION 6
+# Answer:-
+# num1 = 10
+# num2 = 50
+# num3 = 15
+# if (num2>num1) and (num2>num3):
+#     print(f"Num2 is bigger whose value is {num2}")
+# elif (num1>num2) and(num1>num3):
+#     print(f"Num 1 is bigger whose value is {num1}")
+# else:
+#     print(f"Num3 is bigger whose value is {num3}")
+
+
+
+
+
+# QUESTION 7
+# Answer:-
+# value = int(input("Write a number from 1 to 12. "))
+# l1 = ["January", 'February', 'March', 'April', 'May', 'June', 'July','August', 'September'
+#        , 'October', 'November', 'December']
+#
+# l2 = [28, 30, 31]
+# months = l1[value - 1]
+# print(f"This is the month of {months}")
+# if (value==2) :
+#     days = print("It has 28 or 29 days")
+# elif (value == 4) or (value==6) or (value==9) or (value==11) :
+#     print("It has 30 days")
+# else:
+#     print("It has 31 days")
