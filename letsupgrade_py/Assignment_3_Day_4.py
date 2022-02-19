de9bd54017b8efeb676b3effe9a39910322ff1cd
Aarush Kathuria <80425165+aarush-spec@users.noreply.github.com>
2022-02-20 02:27:58 +0530

    Day 4 assignment submitted

---
+++ letsupgrade_py/Assignment_3_Day_4.py
@@ -0,0 +1,25 @@
+# 1
+# Answer :
+x = 50
+y = 100
+if x > y:
+  x, y = y, x
+for i in range(1,x+1):
+  if x%i == 0 and y%i == 0:
+    hcf = i
+
+print("HCF of", x, "and", y, "is:", hcf)
+
+
+# 2
+# answer: c) 0
+
+# 3
+# answer: 4
+
+# 4
+# Answer : a) 10
+#             56
+
+# 5
+# Answer: d)3 2
